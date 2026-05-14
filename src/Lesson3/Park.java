5d538f516d7cb9437df391982a3a8c2cf273cb19
Nastya <anastasiyachern2@gmail.com>
2026-05-14 16:10:58 +0300

    rename folders

--- src/Lesson3/Park.java
+++ src/Lesson3/Park.java
@@ -1,4 +1,4 @@
-package Lesson2;
+package Lesson3;
 
 public class Park {
     public class Attraction {
--- src/Lesson3/Product.java
+++ src/Lesson3/Product.java
@@ -1,4 +1,4 @@
-package Lesson2;
+package Lesson3;
 
 public class Product {
         private String name;
